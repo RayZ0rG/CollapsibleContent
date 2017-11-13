1e5acc9b4eaa3f4a3446911996b251abeae01fba
Ray <raymond.gerz@gmail.com>
2017-11-13 10:17:06 -0500

    built the HTML view files

---
+++ src/shortcode/shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description
+ *
+ * @package       ${NAMESPACE}
+ * @since         1.0.0
+ * @author        RayZ0rG
+ * @link          https://aliensoft.io
+ * @license       GNU-2.0+
+ * /
+// [qc question="What question would you like to ask me?" show_icon="" hide_icon=""]This is the answer[/qa]
+// [teaser visible_message="What question would you like to ask me?" show_icon="" hide_icon=""]This is the answer[/teaser]
